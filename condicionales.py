6dc16513c0a02f986465e6e0e6253f4941a26c3b
munilla1 <mucho-muni@hotmail.com>
2022-04-11 19:56:06 +0200

    2 ejercicios condicionales

---
+++ condicionales.py
@@ -0,0 +1,16 @@
+print ("------------ejercicio numero 1:-------------")
+
+edad = int(input("¿que edad tienes?: "))
+if edad >= 18:
+    print("eres mayor de edad")
+else:
+    print("no eres mayor de edad")
+
+
+print ("------------ejercicio numero 2:-------------")
+
+nombre_clave = input("introduce tu contraseña: ")
+if nombre_clave == "contraseña":
+    print("tu contraseña es correcta")
+else:
+    print("tu contraseña es incorrecta")
